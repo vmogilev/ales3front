07669592f148f3bdb8f89eabeb69e040d62dd6ce
Vitaliy Mogilevskiy <mve@hashjoin.com>
2016-03-17 18:21:16 -0700

    switched to Nanosecond from Millisecond

--- middleware.go
+++ middleware.go
@@ -27,7 +27,8 @@ func logging(next http.Handler) http.Handler {
 			r.Referer(),
 		)
 
-		c.gauge("app.response_time", float64(time.Since(start)/time.Millisecond), nil, 1)
+		//c.gauge("app.response_time", float64(time.Since(start)/time.Millisecond), nil, 1)
+		c.gauge("app.response_time", float64(time.Since(start)), nil, 1)
 	}
 
 	return http.HandlerFunc(fn)
